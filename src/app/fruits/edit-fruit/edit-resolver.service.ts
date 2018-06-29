88cb55a9adb1a50e8467f4b2a20803563da91c9c
botirkhuja <botir90@gmail.com>
2018-06-28 20:29:29 -0500

    Routes with static and dynamic data

---
+++ src/app/fruits/edit-fruit/edit-resolver.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Resolve } from '@angular/router';
+import { LoginService } from '../../core/login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EditResolverService implements Resolve<{loginStatus: boolean, message: string}> {
+
+  constructor(
+    private loginService: LoginService
+  ) { }
+
+  resolve() {
+    return this.loginService.isLoggedIn().then((status: boolean) => {
+      if (status) {
+        return {loginStatus: status, message: "User Logged in"};
+      }
+      return {
+        loginStatus: status,
+        message: "Please log in to edit cart content"
+      }
+    })
+  }
+}
